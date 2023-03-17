1ed5d0b429b326d4faf687b4b8fa2586d3a0223e
hyeongyong-choi <guddyd6761@naver.com>
2023-03-17 16:30:01 +0900

    skeleton

---
+++ skeleton-playground/src/components/skeleton.tsx
@@ -0,0 +1,71 @@
+import React, { useMemo } from 'react';
+import styled from '@emotion/styled/macro'
+import {keyframes ,css} from "@emotion/react";
+
+interface Props {
+    width?: number;
+    height?: number;
+    circle?: boolean;
+    rounded?: boolean;
+    count?: number;
+    unit?: string;
+    animation?: boolean;
+    color?:string;
+    style?:React.CSSProperties
+}
+
+
+const pulseKeyframe = keyframes`
+    0%{
+        opacity: 1;
+    }
+    50%{
+        opacity: 0.4;
+    }
+    100%{
+        opacity:1;
+    }
+`;
+
+
+const pulseAnimation = css`
+    animation: ${pulseKeyframe} 1.5s ease-in-out infinite;
+`
+
+
+const Base = styled.span<Props>`
+    ${({color}) => color && `background-color: ${color}`};
+    ${({rounded}) => rounded && `background-radius: 8px`};
+    ${({circle}) => circle && `background-radius: 50%`};
+    ${({width, height}) => (width || height) && `display: block`};
+    ${({animation}) => animation && pulseAnimation};
+    width: ${({width, unit}) => width && unit && `${width}${unit}`};
+    height: ${({height, unit}) => height && unit && `${height}${unit}`};
+`;
+
+const Content = styled.span`
+    opacity: 0;
+`;
+
+const Skeleton:React.FC<Props> = ({animation=true,width,height,circle,rounded,count,unit = 'px',color = '#F4F4F4',style}) => {
+    const content = useMemo(() => [...Array({length: count})].map(()=>'-').join(''),[count])
+
+    return (
+        <Base
+            style={style}
+            rounded={rounded}
+            animation={animation}
+            width={width}
+            height={height}
+            circle={circle}
+            unit={unit}
+            color={color}
+        >
+            <Content>{content}</Content>
+        </Base>
+    );
+};
+
+
+
+export default Skeleton;
